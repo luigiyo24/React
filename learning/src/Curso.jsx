a63694bd8461cb551343795f35c63bf16f8dc9a1
luigi alonso yovera torres <luigi.yo_24@hotmail.com>
2019-08-18 23:55:37 -0500

    0.3

--- learning/src/Curso.jsx
+++ learning/src/Curso.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+const mayorDeEdad = edad => edad > 18
+
+const persona = {
+  "nombre": "Alberto",
+  "apellido": "Quiroga",
+  "edad": "29"
+}
+
 const Curso = () => (
     <>
     <article className="card">
@@ -8,7 +16,7 @@ const Curso = () => (
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
       <h3 className="t5 s-mb-2 s-center">
-        React.js Desde Cero
+        {mayorDeEdad(persona.edad) ? "Soy mayor de edad" : "Soy menor de edad"}
       </h3>
       <div className="s-mb-2 s-main-center">
         <div className="card__teacher s-cross-center">
@@ -17,53 +25,7 @@ const Curso = () => (
               <img src="https://api.ed.team/files/avatars/38118146-4b4a-4844-8959-157614e04cd1.jpg" alt="" />
             </div>
           </div>
-          <span className="small">Beto Quiroga</span>
-        </div>
-      </div>
-      <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="https://www.google.com">$ 20USD</a>
-      </div>
-    </div>
-  </article>
-  <article className="card">
-    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <img src="https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20Rutas%20peticiones%20hooks.png" alt="" />
-    </div>
-    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-      <h3 className="t5 s-mb-2 s-center">
-        React - Rutas, APIs & Hooks
-      </h3>
-      <div className="s-mb-2 s-main-center">
-        <div className="card__teacher s-cross-center">
-          <div className="card__avatar s-mr-1">
-            <div className="circle img-container">
-              <img src="https://api.ed.team/files/avatars/38118146-4b4a-4844-8959-157614e04cd1.jpg" alt="" />
-            </div>
-          </div>
-          <span className="small">Beto Quiroga</span>
-        </div>
-      </div>
-      <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="https://www.google.com">$ 20USD</a>
-      </div>
-    </div>
-  </article>
-  <article className="card">
-    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <img src="https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20Rutas%20manejo%20de%20estados%20%281%29.png" alt="" />
-    </div>
-    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-      <h3 className="t5 s-mb-2 s-center">
-        React - Manejo del estado de la aplicación
-      </h3>
-      <div className="s-mb-2 s-main-center">
-        <div className="card__teacher s-cross-center">
-          <div className="card__avatar s-mr-1">
-            <div className="circle img-container">
-              <img src="https://api.ed.team/files/avatars/38118146-4b4a-4844-8959-157614e04cd1.jpg" alt="" />
-            </div>
-          </div>
-          <span className="small">Beto Quiroga</span>
+          <span className="small">{`${persona.nombre} ${persona.apellido}`}</span>
         </div>
       </div>
       <div className="s-main-center">
