f004041448f68f8bffa7b303eb330886c80712b9
luigi alonso yovera torres <luigi.yo_24@hotmail.com>
2019-08-19 01:02:04 -0500

    0.4

--- learning/src/Curso.jsx
+++ learning/src/Curso.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-const mayorDeEdad = edad => edad > 18
+const curso = {
+  "title": "React desde cero",
+  "image": "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
+  "price": "50usd"
+}
 
 const persona = {
   "nombre": "Alberto",
@@ -16,20 +20,20 @@ const Curso = () => (
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
       <h3 className="t5 s-mb-2 s-center">
-        {mayorDeEdad(persona.edad) ? "Soy mayor de edad" : "Soy menor de edad"}
+        { curso.title }
       </h3>
       <div className="s-mb-2 s-main-center">
         <div className="card__teacher s-cross-center">
           <div className="card__avatar s-mr-1">
             <div className="circle img-container">
-              <img src="https://api.ed.team/files/avatars/38118146-4b4a-4844-8959-157614e04cd1.jpg" alt="" />
+              <img src={ curso.image } alt={ curso.title } />
             </div>
           </div>
           <span className="small">{`${persona.nombre} ${persona.apellido}`}</span>
         </div>
       </div>
       <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="https://www.google.com">$ 20USD</a>
+        <a className="button--ghost-alert button--tiny" href="https://www.google.com">{ curso.price }</a>
       </div>
     </div>
   </article>
